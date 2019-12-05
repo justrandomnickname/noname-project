bab91a496ee6cea611776e6ff6365ee0b6dcdf8e
justrandomnickname <sasha_r999@mail.ru>
2019-12-05 17:14:37 +0600

    docs: docs updated. Typedoc config file added. Eslintignore updated

---
+++ typedoc.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'file',
+  includeDeclarations: true,
+  tsconfig: 'tsconfig.json',
+  excludePrivate: true,
+  excludeProtected: true,
+  excludeExternals: true,
+  excludeNotExported: false,
+  readme: 'README.md',
+  name: 'empty-yet',
+  ignoreCompilerErrors: true,
+  plugin: 'none',
+  listInvalidSymbolLinks: true,
+}
